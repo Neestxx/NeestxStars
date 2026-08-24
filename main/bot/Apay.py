f32685f5dde3c060af42170e6053655a4c193640
Neestxx <braginmaksim105@gmail.com>
2026-08-25 02:38:00 +0500

    Update Apay.py

--- main/bot/Apay.py
+++ main/bot/Apay.py
@@ -7,11 +7,11 @@ class Apay:
     @staticmethod
     def get_link(amount: int, id_order: str) -> str:
         order_id = id_order
-        secret = "afceefb6-52a1-4995-bb6b-53968f61e805"
+        secret = "Apay secret"
         sign = hashlib.md5(f"{order_id}:{amount}:{secret}".encode()).hexdigest()
 
         params = {
-        "client_id": 1205,
+        "client_id": id клиента,
         "order_id": order_id,
         "amount": amount,
         "sign": sign
@@ -23,11 +23,11 @@ class Apay:
     @staticmethod
     def check_payment(id_order: str) -> str:
         order_id = id_order
-        secret = "afceefb6-52a1-4995-bb6b-53968f61e805"
+        secret = "Apay secret"
         sign = hashlib.md5(f"{order_id}:{secret}".encode()).hexdigest()
 
         params = {
-            "client_id": 1205,
+            "client_id": id клиента,
             "order_id": order_id,
             "sign": sign
         }
